ee32a1ea0e660454de2af6b048498c02b566d89a
darmadevZone <darmadevdone@gmail.com>
2024-07-27 01:14:49 +0900

    add: footer ,header のコンポーネントを作成

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,27 +1,85 @@
-export const darmaSiteInfo = {
-  title: "DarmaT0o0 Site",
-  description: "DarmaT0o0 Site",
-  name: "DarmaT0o0",
-  copylight: {
-    name: "プライバシーについて",
-    link: "",
+import Link from 'next/link';
+import { GithubIcon, TwitterIcon } from './icon';
+
+interface SnsType {
+  name?: string;
+  link: string;
+  icon: string | React.ReactNode;
+  published: boolean;
+}
+
+interface DarmaT0o0SiteInfoType {
+  title: string;
+  description: string;
+  name: string;
+  sns: SnsType[];
+  privacy: {
+    name: string;
+    link: string;
+    published: boolean;
+  };
+}
+
+export const darmaSiteInfo: DarmaT0o0SiteInfoType = {
+  title: 'DarmaT0o0 Site',
+  description: 'DarmaT0o0 Site',
+  name: 'DarmaT0o0',
+  sns: [
+    {
+      name: 'GitHub',
+      link: 'https://github.com/darmadevZone?tab=repositories',
+      icon: <GithubIcon height="20" width="20" />,
+      published: true,
+    },
+    {
+      name: 'Twitter',
+      link: 'https://twitter.com/darmadevZone',
+      icon: <TwitterIcon height="20" width="20" />,
+      published: true,
+    },
+    {
+      name: 'Zenn',
+      link: 'https://zenn.dev/darmadevZone',
+      icon: <TwitterIcon height="20" width="20" />,
+      published: false,
+    },
+  ],
+  privacy: {
+    name: 'プライバシーについて',
+    link: '/privacy',
+    published: false,
   },
 };
 
 const Footer = () => {
   return (
     <footer className="container h-16 border-t">
-      <div className="flex justify-between">
-        <div className="flex flex-col items-center p-2">
+      <div className="flex justify-between mt-2">
+        <div className="flex items-center p-2 gap-3">
           <div>
             <p>
               &copy; 2024
               <span className="p-2 underline">{darmaSiteInfo.name}</span>
             </p>
-            <p> {darmaSiteInfo.copylight.name}</p>
           </div>
+          {darmaSiteInfo.privacy.published && (
+            <div>
+              <p> {darmaSiteInfo.privacy.name}</p>
+            </div>
+          )}
         </div>
-        <div className="flex"></div>
+        <div className="grid grid-cols-3 gap-2">
+          {darmaSiteInfo.sns.map(
+            (sns) =>
+              sns.published && (
+                <Link href={sns.link} key={sns.name}>
+                  <div className="p-2 bg-slate-300 rounded-lg hover:opacity-50">
+                    <p>{sns.icon}</p>
+                  </div>
+                </Link>
+              )
+          )}
+        </div>
       </div>
     </footer>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import { ModeToggle } from "./mode-toggle";
+import Link from 'next/link';
+import { ModeToggle } from './mode-toggle';
 
 interface HeaderNavLink {
   name: string;
@@ -7,32 +7,42 @@ interface HeaderNavLink {
 }
 
 const headerNavLinks: HeaderNavLink[] = [
-  { name: "Home", href: "/" },
-  { name: "About", href: "/about" },
-  { name: "Blog", href: "/blog" },
+  { name: 'Home', href: '/' },
+  { name: 'About', href: '/about' },
+  { name: 'Blog', href: '/blog' },
 ];
 
 const Header = () => {
   return (
-    <header className="container h-16 border-b flex justify-between items-center">
-      <h1 className="text-2xl font-bold">
-        <Link href="/">DarmaT0o0 Site</Link>
-      </h1>
-      <div className="flex items-center space-x-3">
-        <ul className="flex gap-3">
-          {headerNavLinks.map((item) => (
-            <li key={item.name} className="">
-              <Link
-                href={item.href}
-                className="text-lg p-2 rounded-lg  hover:bg-slate-200 hover:font-bold"
-              >
-                {item.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <div>
-          <ModeToggle />
+    <header className=" container h-16 border-b flex justify-between items-center">
+      <div>
+        <h1 className="text-2xl font-bold">
+          <Link href="/">DarmaT0o0</Link>
+        </h1>
+      </div>
+      <div>
+        {/* Mobile Menu Button */}
+        {/* <div className="sm:hidden">
+          <div className="w-8 h-1 bg-slate-600 relative">
+            <div className="w-4 h-1 bg-slate-600 absolute top-4 right-8"></div>
+          </div>
+        </div> */}
+        <div className="hidden sm:flex items-center space-x-3 ">
+          <ul className="flex  gap-3">
+            {headerNavLinks.map((item) => (
+              <li key={item.name}>
+                <Link
+                  href={item.href}
+                  className="text-lg p-2 rounded-lg   hover:bg-slate-200 hover:font-bold hover:opacity-70"
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <ModeToggle />
+          </div>
         </div>
       </div>
     </header>
